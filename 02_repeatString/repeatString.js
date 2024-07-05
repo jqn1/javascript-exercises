59fa22043c998a8753e862cb975d055aff4189b6
jqn1 <154387579+jqn1@users.noreply.github.com>
2024-07-05 19:30:04 -0400

    Complete exercises 1,2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+  if (num < 0) {
+    return "ERROR"
+  }
+  let result = string.repeat(num);
+  return result;
 };
 
 // Do not edit below this line
