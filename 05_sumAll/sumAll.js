9f017f2122a7d49d6c625944a54481f62ae3f465
jqn1 <154387579+jqn1@users.noreply.github.com>
2024-07-07 15:13:35 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+  sum = 0;
+  if (start > end) {
+    aux = start;
+    start = end;
+    end = aux;
+  }
+  if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
+  for (let i = start; i <= end; i++) {
+    sum = sum + i ;
 
+  }
+  return sum;
 };
 
 // Do not edit below this line
