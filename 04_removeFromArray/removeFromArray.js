9e9704c1390d9191a2eadd984c505b88294c77a5
jqn1 <154387579+jqn1@users.noreply.github.com>
2024-07-07 14:43:31 -0400

    Complete exercises 3,4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...remove) {
+  finalArray = []
+  for (let element = 0; element <= array.length-1; element++) {
+    //we push into a new array items that we dont want to remove
+      if (!remove.includes(array[element])) {
+        finalArray.push(array[element]);
+    }
+  }
+  return finalArray;
 };
 
 // Do not edit below this line
