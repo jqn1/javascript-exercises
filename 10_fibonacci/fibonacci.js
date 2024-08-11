ad7c41a5ee80c53037b9c56ddb00dfe870f534b4
jqn1 <154387579+jqn1@users.noreply.github.com>
2024-08-10 22:04:26 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fibonacciArray = [0,1];
+    if (n<0){
+        return "OOPS";
+    }
+    if (n>1){
+        for(let i = 2; i <= n; i++) {
+            fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2];
+
+        }
+    }
+    return fibonacciArray[n];
+
 
 };
 
