3d3e4e0cbbe762ad4852a145376c3c25506e7bb8
jqn1 <154387579+jqn1@users.noreply.github.com>
2024-08-10 22:38:40 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+const findTheOldest = function(people) {
+    oldestPerson = people.reduce((oldest,person) => {
+        let oldestAge;
+        let personAge;
+
+        oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        personAge = person.yearOfDeath - person.yearOfBirth;
+        if (oldest.yearOfDeath === undefined) {
+            oldestAge = currentYear - oldest.yearOfBirth;
+
+        }
+        if (person.yearOfDeath === undefined) {
+            personAge = currentYear - person.yearOfBirth;
+        }
+
+        if (personAge > oldestAge){
+            return person;
+        }else return oldest;
+    })
+    return oldestPerson;
 
 };
 
