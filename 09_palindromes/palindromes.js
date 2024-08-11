057a3c7a7e6e1b6ec940fa1f82867f8b82d3a3d6
jqn1 <154387579+jqn1@users.noreply.github.com>
2024-08-10 21:23:42 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+ 
+    string = string.replace(/[!"#$%&'() *+,-./:;<=>?@[\]^_`{|}~]/g,'').toLowerCase();
+
+
+    let firstHalf = string.slice(0,Math.floor(string.length/2));
+    let secondHalf = string.slice(Math.ceil(string.length/2),string.length);
+
+    // we turn the secondHalf into an array to reverse it 
+    secondHalf = secondHalf.split("").reverse().toString().replaceAll(",","")
+    if (firstHalf === secondHalf){
+        return true;
+    }else return false;
 
 };
 
